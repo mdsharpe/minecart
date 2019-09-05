c8398e6505b494ebafc4a22d9b3e64353bc941d1
Matt Sharpe <mds@gn.apc.org>
2019-09-05 20:57:27 +0100

    cart

--- src/web-ui/src/app/features/game/containers/game-shell/game-shell.component.ts
+++ src/web-ui/src/app/features/game/containers/game-shell/game-shell.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { Engine, Render, Bodies, World, Body, Composites, Composite, Vector, Vertices } from 'matter-js';
+import { Engine, Render, Bodies, World, Body, Composites, Composite, Vector, Vertices, Constraint } from 'matter-js';
 
 @Component({
     selector: 'app-game',
@@ -25,22 +25,25 @@ export class GameShellComponent implements OnInit, OnDestroy {
             // create an engine
             var engine = Engine.create();
 
+            var renderOptions: any = {
+                showAngleIndicator: true
+            };
+
             // create a renderer
             var render = Render.create({
                 element: this._worldContainer.nativeElement,
-                engine: engine
+                engine: engine,
+                options: renderOptions
             });
 
             // create two boxes and a ground
-            var cart = this.createCart(100, 100);
+            var cart = this.createCart(150, 100, 40, 30, 10, 3, 10);
             console.log(cart);
-            var boxA = Bodies.rectangle(400, 200, 80, 80);
-            var boxB = Bodies.rectangle(450, 50, 80, 80);
             var ground = Bodies.rectangle(400, 550, 700, 60, { isStatic: true });
-            Body.rotate(ground, 0.2);
+            Body.rotate(ground, 0.4);
 
             // add all of the bodies to the world
-            World.add(engine.world, [ground, boxA, boxB]);
+            World.add(engine.world, [ground]);
             World.add(engine.world, [cart]);
 
             // run the engine
@@ -56,15 +59,48 @@ export class GameShellComponent implements OnInit, OnDestroy {
         this.unsubscribe$.complete();
     }
 
-    private createCart(x: number, y: number): Composite {
-        const bodyVertices : Vector[] = [
-            { x: 0, y: 0},
-            { x: 5, y: 0}
+    private createCart(
+        x: number,
+        y: number,
+        baseWidth: number,
+        height: number,
+        sideOffset: number,
+        thickness: number,
+        wheelRadius: number
+    ): Composite {
+        const bodyVertices: Vector[] = [
+            { x: 0, y: 0 },
+            { x: thickness, y: 0 },
+            { x: thickness + sideOffset, y: height },
+            { x: thickness + sideOffset + baseWidth, y: height },
+            { x: thickness + sideOffset + baseWidth + sideOffset, y: 0 },
+            { x: thickness + sideOffset + baseWidth + sideOffset + thickness, y: 0 },
+            { x: thickness + sideOffset + baseWidth + thickness, y: height + thickness },
+            { x: sideOffset, y: height + thickness },
         ];
-        const body = Bodies.fromVertices(x, y, [bodyVertices])
+        const body = Bodies.fromVertices(x, y, [bodyVertices]);
+
+        const wheel1 = this.createCartWheel(body.vertices[1].x, body.vertices[1].y, wheelRadius, body);
+        const wheel2 = this.createCartWheel(body.vertices[2].x, body.vertices[2].y, wheelRadius, body);
 
         return Composite.create({
-            bodies: []
+            bodies: [body, wheel1.wheel, wheel2.wheel],
+            constraints: [wheel1.constraint, wheel2.constraint]
         });
+    }
+
+    private createCartWheel(x: number, y: number, radius: number, body: Body): { wheel: Body, constraint: Constraint } {
+        const wheel = Bodies.circle(x, y + (radius * 1.5), radius);
+
+        const bodyCenter = body.position;
+
+        const constraint = Constraint.create({
+            bodyA: body,
+            pointA: { x: x - bodyCenter.x, y: (y - bodyCenter.y) + radius * 1.5 },
+            bodyB: wheel,
+            stiffness: 0.2
+        });
+
+        return { wheel, constraint };
     }
 }
