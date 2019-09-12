29b581ee94d1f39518ba8fee20504bcd9e6fe04b
Matt Sharpe <mds@gn.apc.org>
2019-09-12 19:36:47 +0100

    NPM updates. Stats WIP.

--- src/web-ui/src/app/features/game/game.module.ts
+++ src/web-ui/src/app/features/game/game.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { GameShellComponent } from './containers/game-shell/game-shell.component';
 import { GameRoutingModule } from './game-routing.module';
 import { SharedModule } from 'app/shared';
+import { WorldComponent } from './components/world/world.component';
+import { StatsComponent } from './components/stats/stats.component';
 
 @NgModule({
   imports: [
@@ -10,7 +12,9 @@ import { SharedModule } from 'app/shared';
     SharedModule
   ],
   declarations: [
-    GameShellComponent
+    GameShellComponent,
+    WorldComponent,
+    StatsComponent
   ],
   exports: [GameShellComponent]
 })
