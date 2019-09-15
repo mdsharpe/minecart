a05b8a984b1126200ccf0e805cbec0a4368211a4
Matt Sharpe <mds@gn.apc.org>
2019-09-15 07:35:15 +0100

    Count coins inside cart

--- src/web-ui/src/app/features/game/models/stats-model.ts
+++ src/web-ui/src/app/features/game/models/stats-model.ts
@@ -1,14 +1,14 @@
 import { WorldView } from './world-view';
-import { Bounds } from 'matter-js';
+import { Bounds, Vertices } from 'matter-js';
 
 export class StatsModel {
     constructor(world: WorldView | null) {
         if (world) {
             const x = world.cart.position.x;
-            this.distance = Math.round(x / 100);
+            this.distance = Math.round(x);
             this.velocity = Math.round(world.cart.velocity.x);
             this.coinCoint = world.coins
-                ////.filter(o => Bounds.overlaps(world.cart.vertices, o.vertices))
+                .filter(o => Vertices.contains(world.cart.vertices, o.position))
                 .length;
         } else {
             this.distance = 0;
